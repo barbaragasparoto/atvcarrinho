fc6fca48c985f0e0426e2fedfc261e7a60b818b8
barbaragasparoto <barbaragasparotofigueiredo@gmail.com>
2023-09-18 12:07:19 -0300

    final

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
-import { StyleSheet, StatusBar, SafeAreaView } from 'react-native';
-import Carrinho from './src/pages/Carrinho';
+import {  SafeAreaView, StatusBar, View } from 'react-native';
+import Carrinho from './src/pages/Carrinho'
 
-export default function App() {
+import { useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_400Regular_Italic,
+  Montserrat_700Bold } 
+  from '@expo-google-fonts/montserrat';   
+   
+  export default function App() {
+
+   const[fonteCarregada] = useFonts({
+		"MonteserratRegular" : Montserrat_400Regular,
+    "MontserratBold" : Montserrat_700Bold,
+    "MontserratItalic" : Montserrat_400Regular_Italic
+ 	 })
+
+    if(!fonteCarregada) {
+      return null;
+  }
+
   return (
     <SafeAreaView>
-      <StatusBar/>
+       <StatusBar/>
       <Carrinho/>
     </SafeAreaView>
   );
-}
-
+};
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,22 +1,83 @@
 import React from "react";
-import {Text, Image, StyleSheet, Dimensions} from 'react-native';
-import topo from "../../../assets/imagens/topo.png"
+import {Text, Image, StyleSheet, Dimensions, View} from 'react-native';
+import topo from "../../../assets/imagens/topo.png";
+import anel from "../../../assets/imagens/anel.png"
 
 const width = Dimensions.get('screen').width;
 
 
 export default function Carrinho() {
-    return <>
-    <Image source={topo} style={estilos.topo} />
-    <Text>Teste carrinho dsfgdg</Text>
-    </>
+
+    return<> 
+   <Image source={topo} style={estilos.topo}/>
+   <Text style={estilos.titulo}>Carrinho</Text>
+   <View style={estilos.joia}>
+   <Text style={estilos.nome}>Seus produtos</Text>
+
+   <View style={estilos.loja}>
+        <Image style= {estilos.produto} source= {anel}/>   
+        <Text style={estilos.nomeSapato}>Anel Solitário</Text>
+   </View>
+
+   <Text style={estilos.descricao}>Anel Solitário cravejado com zircônia cristais. Modelo Pandora, joia em prata 925 </Text>
+   
+   <Text style={estilos.preco}>R$ 420,00 </Text>
+
+   </View>
+   </>
 }
 
 const estilos = StyleSheet.create({
+    topo: {
+        marginTop: 40,
+        width: "100%",
+        height: 578/768 * width
+    },
+    titulo: {
+        width: "100%",
+        position: "absolute",
+        color: "black",
+        textAlign: "center",
+        fontSize: 24,
+        lineHeight: 26,
+        fontWeight: "bold",
+        padding: 16,
+        fontFamily: 'MontserratBold'
+    },
+    joia: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
 
-topo: {
-    width: "100%",
-    height: 200
-}
+    },
+    nome: {
+        fontSize: 20,
+        lineHeight: 42,
+        color: "#464646",
+        fontFamily: 'MontserratBold'
+    },
+    loja: {
+        flexDirection: "row",
+        paddingVertical: 12
+    },
+    produto:{
+        width: 32,
+        height: 32
+    },
+    nomeSapato: {
+        fontSize: 16,
+        fontFamily: "MontserratBold",
+        lineHeight: 26,
+        marginLeft: 12
+    },
+    descricao: {
+        color: "#A3A3A3",
+        fontFamily: "MontserratItalic"
+    },
+    preco: {
+        fontSize: 26,
+        fontFamily: "MontserratRegular",
+        color: "#A10000",
+        marginTop: 8
+    }
 
 })
